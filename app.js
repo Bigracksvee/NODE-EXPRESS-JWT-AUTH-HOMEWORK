c2250658d121cefe95577353679e88792b6bc244
Vera <veraopoku100@gmail.com>
2022-08-31 22:28:42 -0400

    jwthomework

---
+++ app.js
@@ -0,0 +1,54 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
+const { checkUser } = require('./views/partials/middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://mean1234:mean1234@node-express-jwt-auth.thlpdlu.mongodb.net/test';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+
+ //cookies
+
+//create a cookie
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-cookie', 'newUser=true');
+
+  //does the same as above, looks cleaner
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24,
+  httpOnly: true });
+  res.send('You got the cookie, check the Application tab in the inspect tool!')
+
+  });
+  
+  //read a cookie
+  
+   app.get('/read-cookies', (req, res) => {
+
+    const cookies = req.cookies;
+    console.log(cookies.newUser);
+    res.json(cookies);
+  
+  });
